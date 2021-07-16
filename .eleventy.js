e325c846a83090da703a44961aa9efd10bfaaac8
Arpit Batra <arpitbatra123@gmail.com>
2021-07-16 13:26:20 +0000

    i don't really understand how responsive images work, but will do one day

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 const Image = require('@11ty/eleventy-img'),
   path = require('path');
 
+// https://www.brycewray.com/posts/2021/04/using-eleventys-official-image-plugin/
 async function imageShortcode(src, alt) {
+  let sizes = '(min-width: 1024px) 100vw, 50vw';
   let srcPrefix = `./assets/images/`;
   src = srcPrefix + src;
   console.log(`Generating image(s) from:  ${src}`);
@@ -10,12 +12,10 @@ async function imageShortcode(src, alt) {
     throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
   }
   let metadata = await Image(src, {
-    formats: ['webp'],
+    formats: ['webp', 'jpeg'],
+    widths: [600, 900, 1500],
     urlPath: '/images/',
     outputDir: './_site/images/',
-    sharpOptions: {
-      quality: 50
-    },
     /* =====
     Now we'll make sure each resulting file's name will 
     make sense to you. **This** is why you need 
@@ -28,16 +28,20 @@ async function imageShortcode(src, alt) {
     }
   });
 
+  let lowsrc = metadata.jpeg[0];
+  let highsrc = metadata.jpeg[metadata.jpeg.length - 1];
   return `<picture>
     ${Object.values(metadata)
       .map((imageFormat) => {
         return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat
           .map((entry) => entry.srcset)
-          .join(', ')}"">`;
+          .join(', ')}" sizes="${sizes}">`;
       })
       .join('\n')}
     <img
-      src="${src}"
+      src="${lowsrc.url}"
+      width="${highsrc.width}"
+      height="${highsrc.height}"
       alt="${alt}"
       loading="lazy"
       decoding="async">
